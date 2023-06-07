7d1dae84cad27dc3a4a1c0901860954450fa0fe1
Mirlok3 <pherafixx@gmail.com>
2023-06-07 17:28:17 +0200

    13 - Styling overhaul - 2

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -4,7 +4,11 @@ Class Home extends Controller
 {
     function index()
     {
+        $DB = new Database();
         $data['page_title'] = "Home";
+
+        $query = "select * from posts order by votes desc limit 3";
+        $data['posts'] = $DB->read($query);
 
         $this->view("home", $data);
     }
--- app/views/header.php
+++ app/views/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="<?= ASSETS ?>/css/style.css">
+    <link rel="stylesheet" href="<?= ASSETS ?>css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title><?= $data['page_title'] . " - " . WEBSITE_NAME ?></title>
 </head>
@@ -41,7 +41,9 @@
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
             <i class="fa fa-bars"></i>
         </a>
+        <?php if(isset($_SESSION['message'])): ?>
+            <p class="message"><?php echo $_SESSION['message']; unset( $_SESSION['message'] );?></p>
+        <?php endif; ?>
     </div>
-    <p class="red"><?php if(isset($_SESSION['message'])): echo $_SESSION['message']; endif; $_SESSION['message'] = ""; ?></p>
 
     <section class="main smoke">
